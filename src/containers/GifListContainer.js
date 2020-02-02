93d932a978d09d03a9dcc9e1eedd5e2f6c2fa523
kevinkim1030 <kevin.kim1030@gmail.com>
2020-02-02 16:03:33 -0500

    Done.

---
+++ src/containers/GifListContainer.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import GifSearch from '../components/GifSearch'
+import GifList from '../components/GifList'
+
+class GifListContainer extends React.Component{
+
+  state = {
+    gifList: []
+  }
+
+  fetchData = (searchInput = "poop") => {
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${searchInput}&api_key=dc6zaTOxFJmzC&rating=r&limit=5`)
+      .then(resp => resp.json())
+      .then(data => this.setState({
+        gifList: data.data
+      })).catch(error => alert("Please try another search"))
+  }
+
+  // componentDidUpdate(prevState){
+  //   if(this.state.searchInput !== prevState.searchInput){
+  //     this.fetchData()
+  //   }
+  // }
+
+  componentDidMount(){
+    this.fetchData()
+  }
+
+
+  render(){
+    return (
+    <div>
+      < GifSearch changeHandler={this.changeHandler} searchInput={this.state.searchInput} fetchData={this.fetchData}/>
+      < GifList gifList={this.state.gifList}/>
+    </div>
+    )
+  }
+
+}
+export default GifListContainer
