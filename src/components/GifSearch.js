0bdd07cf0a643142fbfdf34b8f169c5ed4eb051c
Miss-Cheese <nat.galligan@gmail.com>
2020-02-08 12:27:28 -0500

    Done.

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -6,7 +6,6 @@ function GifSearch (props) {
         <form onSubmit={(event) => props.submitHandler(event)}>
             <input onChange={props.handleChange} value={props.gifQuery} type="text" placeholder="Search"/>
             <input type="submit" value="Find Gif!"/>
-        {console.log(props.gifQuery)}
         </form>
     )
 }
